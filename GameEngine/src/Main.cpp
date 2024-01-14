d329840ebb583f990ff74ceb0d64c16b224a9cdb
AmerA10 <ajami.amer1@gmail.com>
2024-01-13 22:58:56 -0700

    First commit

---
+++ GameEngine/src/Main.cpp
@@ -0,0 +1,15 @@
+// GameEngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+
+int main(int argc, char* argv[])
+{
+    //TODO: Do some magic!
+
+    return 0;
+}
+
+
